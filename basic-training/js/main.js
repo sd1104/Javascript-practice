6b176c56d8c18c4f67852c46db626bf78aa22cbe
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-10-30 11:55:38 +0900

    Update main.js

---
+++ basic-training/js/main.js
@@ -0,0 +1,38 @@
+// ★★オブジェクト★★
+// 次のような'boy'という名前のオブジェクトを定義しましょう。
+// 名前が'johon'
+// 年齢が20
+// 趣味が'fishing'
+// sayという関数を使うとコンソールに'hello'と表示する
+
+
+// ★★繰り返し★★
+// 1〜10までの数値を順番に足し合わせて、最後に全て足した結果をターミナルに出力するプログラムを書いてください。
+
+
+// 以下の配列から、数を探して何番目に含まれているか結果を返すメソッドをsearchとeach_with_indexを用いて作成してください。
+// let array = [3, 5, 9 ,12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93];
+
+// （使用例）
+
+// search(12, array);
+// => 4番目にあります
+
+// search(7, array);
+// => その数は含まれていません
+
+
+// ★★条件分岐★★
+// あなたは警官です。aとb二人の容疑者の取り調べをしています。どちらも証言がTrue、またはFalseであればその証言はTrueです。しかしどちらかがFalseでTrueであればその証言はFalse、と出力するメソッドを論理演算子を用いて作りましょう。
+// 呼び出し方：
+// police_trouble(a, b)
+// 出力例：
+// police_trouble(true, false) → Falsepolice_trouble(false, false) → Truepolice_trouble(true, true) → True
+
+
+// ★★関数★★
+// 任意の文字に対してn番目の文字を消し、その消した文字を出力するメソッドを作りましょう。
+// 呼び出し方
+// missing_char(string, num)
+// 出力例：
+// missing_char('kitten', 1) → 'itten'missing_char('kitten', 2) → 'ktten'missing_char('kitten', 4) → 'kiten'
