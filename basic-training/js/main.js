14fe6eba28a82950c4935f276602fef4fb81ba4d
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-01 11:59:18 +0900

    Update main.js

--- basic-training/js/main.js
+++ basic-training/js/main.js
@@ -16,17 +16,6 @@
 // sum_up(num);
 
 
-// 以下の配列から、数を探して何番目に含まれているか結果を返すメソッドをsearchとeach_with_indexを用いて作成してください。
-// let array = [3, 5, 9 ,12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93];
-
-// （使用例）
-
-// search(12, array);
-// => 4番目にあります
-
-// search(7, array);
-// => その数は含まれていません
-
 
 // ★★条件分岐★★
 // あなたは警官です。aとb二人の容疑者の取り調べをしています。どちらも証言がTrue、またはFalseであればその証言はTrueです。しかしどちらかがFalseでTrueであればその証言はFalse、と出力するメソッドを論理演算子を用いて作りましょう。
@@ -36,26 +25,3 @@
 // police_trouble(true, false) → False
 // police_trouble(false, false) → True
 // police_trouble(true, true) → True
-
-let A = 1;
-let B = 2;
-
-let police_troubule = function(a,b) {
-  if( a === b) {
-    console.log('YES');
-  } else if( a !== B) {
-    console.log('NO');
-  } else {
-    console.log('ERROR')
-  }
-}
-
-police_troubule(A,B);
-
-
-// ★★関数★★
-// 任意の文字に対してn番目の文字を消し、その消した文字を出力するメソッドを作りましょう。
-// 呼び出し方
-// missing_char(string, num)
-// 出力例：
-// missing_char('kitten', 1) → 'itten'missing_char('kitten', 2) → 'ktten'missing_char('kitten', 4) → 'kiten'
