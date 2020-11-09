c2fd9c8f8b1f052efec45752d0bdd493d304e38f
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-09 17:44:12 +0900

    Update main.js

--- basic-training/js/main.js
+++ basic-training/js/main.js
@@ -99,3 +99,18 @@ let boys = {
 // console.log(d('Mike', 'John'));
 // console.log(e());
 // console.log(f());
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  hello() {
+    console.log('hello' + ' ' + this.name);
+  }
+}
+
+const bob = new Person('Bob', 20);
+console.log(bob.name);
+bob.hello();
