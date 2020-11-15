1f1877cec51eb613bbc5e3a0b51a2337ae69d243
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-11-15 10:46:50 +0900

    Update main.js

--- basic-training/js/main.js
+++ basic-training/js/main.js
@@ -6,3 +6,27 @@ fruit.unshift('peach');
 fruit.push('melon');
 
 fruit[1] = 'grapefruit';
+
+let boys = {
+  john: {
+    age: 20,
+    hobby: 'fishing',
+    say: function() {
+      return 'hello';
+    }
+  },
+  mike: {
+    age: 21,
+    hobby: 'singing',
+    say: function() {
+      return 'bye';
+    }
+  },
+  yoda: {
+    age: 200,
+    hobby: 'growing',
+    say: function() {
+      return 'hoho';
+    }
+  }
+}
